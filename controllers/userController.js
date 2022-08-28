e34101d65743bbeb2ba9a39d0919fbfa2d6f7bbe
Sevde <sevdeorscelik@gmail.com>
2022-08-28 23:45:45 +0200

    authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from 'bcrypt'
-
+import jwt from 'jsonwebtoken'
 
 const createUser = async (req, res) => {
     try {
@@ -42,7 +42,10 @@ const loginUser = async (req, res) => {
 
     // sifrenin uyusup uyusmadigini kontrol edilir
         if (same) {
-            res.status(200).send("You are loggend in")
+            res.status(200).json({
+                user,
+                token: createToken(user._id),
+            })
         } else {
             res.status(401).json({
                 succeded: false,
@@ -57,6 +60,13 @@ const loginUser = async (req, res) => {
         });
     };
 }
+
+
+const createToken = (userId) => {
+    return jwt.sign({userId},process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    })
+};
 
 
 export {
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+import User from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
+
+
+
+
+const authenticateToken = async (req, res, next) => {
+
+
+    try {
+
+        const authHeader = req.headers["authorization"]
+        const token = authHeader && authHeader.split(" ")[1]
+
+        if (!token) {
+            return res.status(401).json({
+                succeeded: false,
+                error: 'No token available',
+            })
+        }
+
+        req.user = await User.findById(
+            jwt.verify(token, process.env.JWT_SECRET).userId
+        )
+
+        next();
+
+    } catch (error) {
+        res.status(401).json({
+            succeeded: false,
+            error: 'Not authorized'
+
+        })
+    }
+
+}
+
+export { authenticateToken }
